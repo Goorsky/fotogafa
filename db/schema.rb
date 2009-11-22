2189b7b6d1e53b3f3d386969751cfbd2792947d1
Rafal Hirsz <hirszdesign@gmail.com>
2009-11-22 21:30:50 +0100

    Precz z NetBeansem!

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091121175007) do
+ActiveRecord::Schema.define(:version => 20091122171408) do
 
   create_table "albums", :force => true do |t|
     t.integer  "parent_id"
     t.integer  "user_id"
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", :force => true do |t|
+    t.string   "title"
+    t.integer  "album_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
