1b17a4e515ca5b603d9d3fc7efa49c319e81bfc0
Michal Gorski (Goorsky) <goorsky@3is.pl>
2009-11-22 23:33:19 +0100

    helper do ladnych PL bledow

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
   has_many :photos
-  validates_presence_of :title, :message => "Podaj tytuł"
+  validates_presence_of :title, :message => "Wprowadź nazwę albumu"
+  validates_presence_of :description, :message => "Wprowadź opis dla albumu"
 end
